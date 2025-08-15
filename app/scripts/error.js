9c42a842df8a1220232fb7b8331e8e16a6058177
exerinity <me@ex3.lol>
2025-08-15 21:18:28 +1000

    add success box

--- app/scripts/error.js
+++ app/scripts/error.js
@@ -8,17 +8,17 @@ err_container.style.flexDirection = 'column-reverse';
 err_container.style.gap = '10px';
 document.body.appendChild(err_container);
 
-function throw_error(msg) {
+function throw_error(msg, success = false) {
     const err_box = document.createElement('div');
     err_box.className = 'error-box';
-    err_box.style.background = '#a00';
+    err_box.style.background = success ? '#238600' : '#860000';
     err_box.style.color = 'white';
     err_box.style.padding = '10px 20px';
     err_box.style.borderRadius = '4px';
     err_box.style.boxShadow = '0 2px 5px rgba(0, 0, 0, 0.2)';
     err_box.style.opacity = '1';
     err_box.style.transition = 'opacity 1s ease';
-    err_box.innerHTML = `<i class="fa-solid fa-triangle-exclamation"></i> ${msg}`;
+    err_box.innerHTML = `<i class="fa-solid ${success ? 'fa-check' : 'fa-triangle-exclamation'}"></i> ${msg}`;
 
     err_container.appendChild(err_box);
 
@@ -33,8 +33,10 @@ function throw_error(msg) {
         err_container.firstChild.remove();
     }
 
-    elements.error_sound.currentTime = 0;
-    elements.error_sound.play();
+    if (!success) {
+        elements.error_sound.currentTime = 0;
+        elements.error_sound.play();
+    }
 
-    console.error(msg);
+    if (!success) console.error(msg);
 }
