216e55d09871e4b945f6d3f2e0234d7e04a7aa6d
exerinity <me@ex3.lol>
2025-08-18 14:50:52 +1000

    stabilizing the scrolling title

--- app/scripts/title.js
+++ app/scripts/title.js
@@ -1,13 +1,21 @@
 const wm = document.querySelector('title');
 let now = 0;
-
+// This is shitty, but hopefully works
 function tabtitle() {
     const scroll = [
-        metadata.title,
-        metadata.artist,
-        metadata.album,
+        metadata.title || 'Audion',
+        metadata.artist || 'Audion',
+        metadata.album || 'Audion',
         `Audion`
     ];
     wm.textContent = scroll[now];
     now = (now + 1) % scroll.length;
 }
+
+setInterval(tabtitle, 5000);
+tabtitle();
+
+/* This script is inspired by Windows Media Player's title bar, most prominently the miniplayer, on XP and above
+   (I probably got that wrong, but I seen this on XP, 7, and 10)
+   What I mean: https://i.exerinity.com/wmp.gif
+*/
