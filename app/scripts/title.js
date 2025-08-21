ea5a7bb1990c6868b1a08f80f3bb361b25ae3cbd
exerinity <me@ex3.lol>
2025-08-21 23:31:29 +1000

    handle singles with eponymous album names

--- app/scripts/title.js
+++ app/scripts/title.js
@@ -1,21 +1,25 @@
-const wm = document.querySelector('title');
 let now = 0;
-// This is shitty, but hopefully works
+
 function tabtitle() {
-    const scroll = [
-        metadata.title || 'Audion',
-        metadata.artist || 'Audion',
-        metadata.album || 'Audion',
-        `Audion`
-    ];
-    wm.textContent = scroll[now];
+    const scroll = [];
+
+    if (typeof metadata === "object" && metadata) {
+        const title = metadata.title || "Audion";
+        const artist = metadata.artist || "Audion";
+        const album = metadata.album || "Audion";
+
+        if (title === album) {
+            scroll.push(title, artist, "Audion");
+        } else {
+            scroll.push(title, artist, album, "Audion");
+        }
+    } else {
+        scroll.push("Audion");
+    }
+
+    document.title = scroll[now];
     now = (now + 1) % scroll.length;
 }
 
-setInterval(tabtitle, 5000);
 tabtitle();
-
-/* This script is inspired by Windows Media Player's title bar, most prominently the miniplayer, on XP and above
-   (I probably got that wrong, but I seen this on XP, 7, and 10)
-   What I mean: https://i.exerinity.com/wmp.gif
-*/
+setInterval(tabtitle, 5000);
